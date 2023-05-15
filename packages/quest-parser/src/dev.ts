3b1b3c6f5d836e46d4175dc78e9a39b5d50eb581
Jan Bayer <enderjanyt@gmail.com>
2023-05-15 21:56:38 +0200

    fix up parser, can now fully parse Create: Astral

--- packages/quest-parser/src/dev.ts
+++ packages/quest-parser/src/dev.ts
@@ -1,6 +1,19 @@
-import { readFile } from "fs/promises";
-import { QuestParser } from "./quest-parser.js";
+import { lstat, readFile, readdir } from "fs/promises";
+import snbtToJS from "./quest-parser.js";
 
 const content: Buffer = await readFile("../../../Create-Astral/config/ftbquests/data.snbt");
 
-const parser: QuestParser = new QuestParser(String(content));
+parseDirectory("../../../Create-Astral/config/ftbquests/")
+
+async function parseDirectory(baseDir: string) {
+    for (let file of await readdir(baseDir)) {
+        const fileData = (await lstat(baseDir + file));
+        if (fileData.isDirectory()) {
+            await parseDirectory(baseDir + file + "/");
+        } else {
+            console.log(file);
+            console.log(snbtToJS(String(await readFile(baseDir + file)), file));
+        }
+    }
+}
+
--- packages/quest-parser/src/quest-parser.ts
+++ packages/quest-parser/src/quest-parser.ts
@@ -1,18 +1,16 @@
-export class QuestParser {
-    constructor(private snbt: string) {
-        console.log(QuestParser.snbtToJS(snbt));
-    }
+import { writeFileSync } from "fs";
 
-    private static snbtToJS(snbt: string): any {
-        // add double quotes around identifiers
-        let jsonStr = snbt.replace(/(?<!"\w*)(\w+:)|(\w+ :)/g, (matchedStr) => '"' + matchedStr.substring(0, matchedStr.length - 1) + '":');
-        // remove type decorator from floating point numbers
-        jsonStr = jsonStr.replace(/\d+(\.\d+)?[dfbL]/g, str => str.substring(0, str.length - 1));
-        // add commas after each line
-        jsonStr = jsonStr.split("\n").filter(str => str.length > 0).map(str => str.charAt(str.length - 1) === "{" || str.length < 2 ? str : (str + ",")).join("\n");
-        // remove trailing commas
-        jsonStr = jsonStr.replace(/\,(?!\s*?[\{\[\"\'\w])/g, '');
-    
-        return JSON.parse(jsonStr);
-    }
+export default function snbtToJS(snbt: string, filename = "out"): any {
+    // add double quotes around identifiers
+    let jsonStr = snbt.replace(/(?<!".*)(\w+:)|(\w+ :)/g, (matchedStr) => '"' + matchedStr.substring(0, matchedStr.length - 1) + '":');
+    // remove type decorator from floating point numbers
+    jsonStr = jsonStr.replace(/\d+(\.\d+)?[dfbsL]/g, str => str.substring(0, str.length - 1));
+    // add commas after each line
+    jsonStr = jsonStr.split("\n").filter(str => str.length > 0).map(str => str.charAt(str.length - 1) === "[" || str.charAt(str.length - 1) === "{" || str.length < 2 ? str : (str + ",")).join("\n");
+    // remove trailing commas
+    jsonStr = jsonStr.replace(/\,(?!\s*?[\{\[\"\'\w])/g, '');
+
+    writeFileSync("./generated_json/" + filename + ".json", jsonStr);
+
+    return JSON.parse(jsonStr);
 }
