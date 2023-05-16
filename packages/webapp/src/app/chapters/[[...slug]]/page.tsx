1b2ae8aacefb611362592196e1d713df9fb83a0d
Jan Bayer <enderjanyt@gmail.com>
2023-05-16 20:09:16 +0200

    use process env to load chapters

--- packages/webapp/src/app/chapters/[[...slug]]/page.tsx
+++ packages/webapp/src/app/chapters/[[...slug]]/page.tsx
@@ -6,8 +6,12 @@ import { cache } from 'react';
 import {notFound} from "next/navigation";
 
 const read = cache(async (filename: string[]) => {
-  console.log("reading", filename);
-  return snbtToJS(String(await readFile("../../../Create-Astral/config/ftbquests/quests/chapters/" + filename.join("/"))));
+  console.log("reading", process.env.CHAPTER_BASEDIR, filename);
+  if (!process.env.CHAPTER_BASEDIR) {
+    console.error("CHAPTER_BASEDIR Environemnt Variable not specified");
+    return null;
+  }
+  return snbtToJS(String(await readFile(process.env.CHAPTER_BASEDIR + filename.join("/"))));
 });
 
 export default async function Home({params}: {params: {slug: string[]}}) {
